f0cd16177f3628eb2ad027fe94b36ffd89ef6b88
QuatroQuatros <guicossalter@gmail.com>
2024-09-06 00:52:05 -0300

    feat:wip Criação da classe responsavel por tratar exceções

---
+++ src/main/java/com/challangeLocaweb/api/exceptions/ModelNotFoundException.java
@@ -0,0 +1,16 @@
+package com.challangeLocaweb.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ModelNotFoundException extends RuntimeException{
+
+    public ModelNotFoundException(String message){
+        super(message);
+    }
+
+    public ModelNotFoundException() {
+
+    }
+}
