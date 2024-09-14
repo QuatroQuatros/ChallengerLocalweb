42e2cc4cd1c5a39db9b650acfaa106649724703c
QuatroQuatros <guicossalter@gmail.com>
2024-09-14 01:11:32 -0300

    feat:wip new exception for handler non authenticated users

---
+++ src/main/java/com/challangeLocaweb/api/exceptions/UserNotAuthenticatedException.java
@@ -0,0 +1,12 @@
+package com.challangeLocaweb.api.exceptions;
+
+public class UserNotAuthenticatedException extends RuntimeException {
+
+    public UserNotAuthenticatedException(String message) {
+        super(message);
+    }
+
+    public UserNotAuthenticatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
