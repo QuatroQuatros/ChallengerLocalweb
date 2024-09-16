eca2bcbfbcb43a72348042d7adc69cfcd85944bc
Guilherme Lima <gui.limasoares@hotmail.com>
2024-09-15 23:13:20 -0300

    modify in seed_system_labels

--- src/main/resources/db/migrations/V6__seed-system-labels.sql
+++ src/main/resources/db/migrations/V6__seed-system-labels.sql
@@ -2,9 +2,7 @@
 INSERT INTO system_labels (label_name, created_at, update_at) VALUES
                             ('label.favoritos', NOW(), NOW()),
                             ('label.spam', NOW(), NOW()),
-                            ('label.fiap', NOW(), NOW()),
-                            ('label.alura', NOW(), NOW()),
-                            ('label.vagas', NOW(), NOW()),
-                            ('label.interesses', NOW(), NOW()),
+                            ('label.enviados', NOW(), NOW()),
+                            ('label.rascunhos', NOW(), NOW()),
                             ('label.lixeira', NOW(), NOW()),
                             ('label.naoLido', NOW(), NOW());
